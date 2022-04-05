1e599407eafb321ef9e3a5573c3558c4a583343d
DezlowNG <dezlow@bk.ru>
2022-04-05 17:42:27 +0700

    Added random and some fixes

---
+++ Engine/Source/Core/Random.cpp
@@ -0,0 +1,11 @@
+//
+// Copyright (c) Oneiro Games. All rights reserved.
+// Licensed under the GNU General Public License, Version 3.0.
+//
+
+#include "Oneiro/Core/Random.hpp"
+
+namespace oe::Core
+{
+	std::mt19937 Random::mGenerator{};
+}
