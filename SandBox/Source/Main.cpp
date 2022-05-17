436a61ce40c1cca61ff0a921be6888efa570a31d
Dezlow <dezlow@bk.ru>
2022-05-17 08:01:22 +0700

    Removed VNApplication.hpp

--- SandBox/Source/Main.cpp
+++ SandBox/Source/Main.cpp
@@ -3,4 +3,47 @@
 // Licensed under the GNU General Public License, Version 3.0.
 //
 
-#include "Oneiro/VisualNovel/VNApplication.hpp"
+#include "Oneiro/Runtime/Application.hpp"
+#include "Oneiro/VisualNovel/VNCore.hpp"
+#include "Oneiro/Lua/LuaFile.hpp"
+
+namespace SandBox
+{
+    class Application : public oe::Runtime::Application
+    {
+    public:
+        bool Init() override
+        {
+            mFile.LoadFile("Assets/Scripts/test.lua", false);
+            oe::VisualNovel::Init(&mFile);
+            return true;
+        }
+
+        bool Update() override
+        {
+            oe::VisualNovel::Update();
+            return true;
+        }
+
+        void HandleButton(oe::Input::Button button, oe::Input::Action action) override
+        {
+            if (button == oe::Input::Button::LEFT && action == oe::Input::Action::PRESS)
+                oe::VisualNovel::NextStep();
+        }
+
+        void Shutdown() override
+        {
+
+        }
+    private:
+        oe::Lua::File mFile;
+    };
+}
+
+namespace oe::Runtime
+{
+    std::shared_ptr<Application> CreateApplication(int, char* [])
+    {
+        return std::make_shared<SandBox::Application>();
+    }
+}
