649abe8986c5f6ded4c9399812df20d25478cc4f
Dezlow <dezlow@bk.ru>
2022-06-25 11:22:32 +0700

    Added app header in SandBox proj and moved funcs impl into src file

---
+++ SandBox/Source/SandBoxApp.hpp
@@ -0,0 +1,31 @@
+//
+// Copyright (c) Oneiro Games. All rights reserved.
+// Licensed under the GNU General Public License, Version 3.0.
+//
+
+#pragma once
+
+#include "Oneiro/Runtime/Application.hpp"
+#include "Oneiro/World/Components.hpp"
+#include "Oneiro/World/Entity.hpp"
+
+namespace SandBox
+{
+    class Application final : public oe::Runtime::Application
+    {
+        using oe::Runtime::Application::Application;
+    public:
+        bool OnInit() override;
+        bool OnUpdate(float deltaTime) override;
+        void MousePos(double xPos, double yPos) override;
+        void OnShutdown() override;
+    private:
+        std::shared_ptr<oe::World::World> mWorld{};
+        oe::Renderer::GL::Shader mShader{};
+    };
+}
+
+namespace oe::Runtime
+{
+    std::shared_ptr<Application> CreateApplication(int, char*[]);
+}
